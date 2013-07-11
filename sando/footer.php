f66907dfb4838a465ad07bf3f7dbfdc0d1a790c6
myhyazid <myh.myh09@yahoo.com>
2013-07-11 22:44:19 +0800

    Update footer.php
    Add Site Name instead of Sando in the footer

--- sando/footer.php
+++ sando/footer.php
@@ -3,7 +3,7 @@
         <footer>
         	<div class="wrapper">
             	<a id="anchor_logo" href="//anchorcms.com">Powered by Anchor</a>
-            	<p>Copyright &copy; <?php echo date('Y'); ?> Sando. All rights reserved.<br>Theme by <a href="//twitter.com/cosminnegoita">Cosmin Negoita</a></p>
+            	<p>Copyright &copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.<br>Theme by <a href="//twitter.com/cosminnegoita">Cosmin Negoita</a></p>
             </div>
         </footer>
     </body>
